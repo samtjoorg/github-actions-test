cb4f14ae77746fd6109e064c726e292919b630e7
Sam Johnson <sam.johnson@sage.com>
2021-11-05 14:45:39 +0000

    feat: add chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+  refs: {
+    test: {
+      title: "Sam test",
+      url:
+        "https://carbon-versioned.s3.eu-west-1.amazonaws.com/archive/1.0.14/index.html",
+    },
+  },
+};
